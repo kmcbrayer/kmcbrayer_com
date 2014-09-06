98db845de59293c5ca6502b869702179a7c6c5ad
kmcbrayer <kmcbrayer77@gmail.com>
2014-09-05 22:50:10 -0400

    init commit

---
+++ kmcbrayer_com/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^api/', include('api.urls'))
+)

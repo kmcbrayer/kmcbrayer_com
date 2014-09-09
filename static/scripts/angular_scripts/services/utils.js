0f1663677b36a6a09507c0c422efd2da31eecd57
kmcbrayer <kmcbrayer77@gmail.com>
2014-09-09 04:20:07 -0400

    started adding regustrator live edit

--- static/scripts/angular_scripts/services/utils.js
+++ static/scripts/angular_scripts/services/utils.js
@@ -56,8 +56,7 @@ angular.module('regUstratorApp')
         sceneData.camera.position.x = 0;
         sceneData.camera.position.y = 3;
         sceneData.camera.position.z = 10;
-        
-        sceneData.controls = new THREE.OrbitControls(sceneData.camera);
+        sceneData.controls = new THREE.OrbitControls(sceneData.camera,document.getElementById('window'));
         
       },
       cleanCam: function(){

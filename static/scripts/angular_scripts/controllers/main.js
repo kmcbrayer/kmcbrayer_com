f616ace25316c275b28f5b8c810312ec87c96cc8
Kevin McBrayer <kmcbrayer77@gmail.com>
2014-09-23 15:22:57 -0400

    fixed text adding for regUpreview

--- static/scripts/angular_scripts/controllers/main.js
+++ static/scripts/angular_scripts/controllers/main.js
@@ -5,10 +5,11 @@ angular.module('regUstratorApp')
               boxDefaults, lineDefaults,triangleDefaults,textDefaults) {
     sceneData.cameraView = 'perspective';
     $scope.textAreaString = textAreaService.textAreaString;
-    $scope.eleList = [
+    $scope.eleTypeList = [
       {'name':'Box'},{'name':'Line'},{'name':'Triangle'},{'name':'Text'}
     ];
-    $scope.eleSelected = $scope.eleList[0];
+    $scope.eleSelected = $scope.eleTypeList[0];
+    $scope.elementList = []; 
 
     $scope.boxDefaults = boxDefaults;
     $scope.lineDefaults = lineDefaults;
@@ -45,9 +46,10 @@ angular.module('regUstratorApp')
       if ($scope.eleSelected.name === 'Text') {
         //get input names and values
         var base = "<text options=\"{";
-        var end = "}\"></text>";
+        var end1 = "}\">";
+        var end2 = "</text>";
         var elements = $("#textOptions :input");
-        $scope.addString(elements,base,end);
+        $scope.addTextString(elements,base,end1,end2);
       }
     };
     $scope.clearElements = function() {
@@ -55,11 +57,22 @@ angular.module('regUstratorApp')
       textAreaService.textAreaString = $scope.textAreaString;
       $rootScope.$broadcast('textUpdated');
     };
-
+    $scope.addTextString = function(elements,base, end1,end2) {
+      var ops = '';
+      var innerText = 'default';
+      elements.each(function(i){
+        if ($(this).context.name == 'text'){
+          innerText = $(this).context.value;
+        }
+        ops += "\'"+$(this).context.name+"\' : "+$(this).context.value+",";
+      })
+      $scope.textAreaString += base+ops.substr(0,ops.length-1)+end1+innerText+end2;//trim last comma
+      $scope.submitCode();
+    }
     $scope.addString = function(elements, base,end) {
       var ops = '';
       elements.each(function(i){
-        ops += "\'"+$(this).context.name+"\' : "+$(this).context.value+","
+        ops += "\'"+$(this).context.name+"\' : "+$(this).context.value+",";
       });
       $scope.textAreaString += base+ops.substr(0,ops.length-1)+end;//trim last comma
       $scope.submitCode();
